2b6cca8a77a62a2cd18e341351be67815cd54b8b
OdilovMM <dssrinman@gmail.com>
2025-06-25 12:05:44 +0900

    feat: product update api fix adn changes

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -78,13 +78,9 @@ class AdminController {
 		try {
 			const data = req.body;
 			const { id } = req.params;
-			const userId = this.userId;
-			const user = await userModel.findById(userId);
-			if (!user) return res.json({ failure: 'User not found' });
-			if (user.role !== 'admin') return res.json({ failure: 'User is not admin' });
 			const updatedProduct = await productModel.findByIdAndUpdate(id, data);
 			if (!updatedProduct) return res.json({ failure: 'Failed while updating product' });
-			return res.json({ success: 'Product updated successfully' });
+			return res.json({ status: 200 });
 		} catch (error) {
 			next(error);
 		}
